b67647ba2cefbede39f013e1f3e091b74574f68c
epumeriribarren <informatico@barcelonaled.com>
2020-04-24 16:10:36 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bootstrap Override',
+    'category': 'Hidden',
+    'version': '1.0',
+    'autor': 'Epumer Pehuen Iribarren Inchausti',
+    'description':
+        """
+        Este módulo permite editar el estilo del backend
+        """,
+    'depends': ['web'],
+    'auto_install': False,
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+}
